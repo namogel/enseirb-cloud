b7faad6fb2749b8c022ff07512f1b24ec8d7d624
benjamin <bsaintes@enseirb-matmeca.fr>
2014-11-26 12:36:41 +0100

    serveur flask temporaire pour répondre aux demandes de login + décentralisation du log du serveur django

--- django-server/app/views.py
+++ django-server/app/views.py
@@ -5,7 +5,10 @@ from annoying.decorators import render_to
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+import requests
 
+
+SERVER_BASE_URL = 'http://localhost:8080/'
 
 def index(request):
     return HttpResponse("hi there")
@@ -13,42 +16,58 @@ def index(request):
 
 @render_to('login.html')
 def login_user(request):
-    context = {'form_login': {}, 'wrong_login': False, 'wrong_username': False, 'form_create': {}, 'username_busy': False}
+    context = {'form_login': {}, 'wrong_login': False, 'wrong_username': False, 'form_create': {}, 'username_busy': False, 'invalid_mail': False, 'mail_busy': False}
     if request.method == 'POST':
         try:
             action = request.POST['action']
             username = request.POST['username']
             password = request.POST['password']
-            if action == 'login':
+            data = {
+                'option': action,
+                'username': username,
+                'password': password,
+                'mail': request.POST.get('mail', None),
+            }
+            login_req = requests.post(SERVER_BASE_URL + 'login', data=data)
+            login_status = login_req.text
+            print login_status
+
+            if action == 'connect':
                 context['form_login'] = request.POST
-                try:
-                    User.objects.get(username=username)
+                if login_status == 'ok':
+                    try:
+                        User.objects.get(username=username)
+                    except User.DoesNotExist:
+                        # User has not been created yet on Django server db
+                        User.objects.create_user(username, password=password)
                     user = authenticate(username=username, password=password)
-                    if user != None:
-                        login(request, user)
-                        print 'next: ', request.POST['next']
-                        return redirect(reverse('app.views.home'))
-                    else:
-                        context['wrong_login'] = True
-                except User.DoesNotExist:
+                    login(request, user)
+                    return redirect(reverse('app.views.home'))
+                elif login_status == 'invalid_username':
                     context['wrong_username'] = True
+                elif login_status == 'invalid_password':
+                    context['wrong_login'] = True
+
             elif action == 'create':
-                try:
-                    User.objects.get(username=username)
-                except User.DoesNotExist:
+                if login_status == 'ok':
                     User.objects.create_user(username, password=password)
                     user = authenticate(username=username, password=password)
                     login(request, user)
-                    print 'next: ', request.POST['next']
                     return redirect(reverse('app.views.home'))
                 else:
-                    context['username_busy'] = True
+                    if login_status == 'username_busy':
+                        context['username_busy'] = True
+                    if login_status == 'invalid_mail':
+                        context['invalid_mail'] = True
+                    if login_status == 'mail_busy':
+                        context['mail_busy'] = True
                     context['form_create'] = request.POST
+
         except KeyError:
             return HttpResponseBadRequest
 
     return context
-    
+
 
 def logout_user(request):
     logout(request)
---
+++ flask-server/server.py
@@ -0,0 +1,138 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+
+# configuration
+app = Flask(__name__)
+app.config.update(dict(
+    DATABASE='database.db',
+    DEBUG=True,
+    SECRET_KEY='1234',
+    USERNAME='admin',
+    PASSWORD='admin'
+))
+
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+# def query_db(query, args=(), one=False):
+#     cur = get_db().execute(query, args)
+#     rv = cur.fetchall()
+#     cur.close()
+#     return (rv[0] if rv else None) if one else rv
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print 'Initialized the database'
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+#####################################################################################
+
+
+PROTOCOL = {
+    'login': ['connect', 'create', 'update', 'delete'],
+    'data': ['read', 'upload', 'download', 'update', 'delete'],
+    }
+
+
+def add_db_user(username, password, mail):
+    db = get_db()
+    db.execute('insert into users (username, password, mail) values (?, ?, ?)',
+               [username, password, mail])
+    db.commit()
+    flash('New entry was successfully posted')
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    add_db_user(request.form['username'], request.form['password'], request.form['mail'])
+    return redirect(url_for('home'))
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    db = get_db()
+    db.execute('delete from users where id = ?', [request.form['id']])
+    db.commit()
+    return redirect(url_for('home'))
+
+@app.route('/search_user', methods=['POST'])
+def search_user():
+    users = get_db_users(request.form['username'], request.form['mail'])
+    return render_template('index.html', users=users)
+
+def get_db_users(username, mail):
+    db = get_db()
+    if username != '' or mail != '':
+        cur = db.execute('select * from users where username = ? or mail = ?', [username, mail])
+    else:
+        cur = db.execute('select * from users')
+    users = cur.fetchall()
+    return users
+
+@app.route('/', methods=['GET'])
+def home():
+    db = get_db()
+    cur = db.execute('select * from users')
+    users = cur.fetchall()
+    return render_template('index.html', users=users)
+
+def is_request_valid(type, form):
+    try:
+        request_option = form['option']
+        options = PROTOCOL['login']
+        if not request_option in options:
+            raise KeyError
+    except KeyError:
+        return False
+    return True
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not is_request_valid('login', request.form):
+        return "invalid protocol"
+    
+    username = request.form['username']
+    password = request.form['password']
+    
+    if request.form['option'] == 'connect':
+        user = get_db_users(username, '')
+        if user == []:
+            return 'invalid_username'
+        if password != user[0][2]:
+            return 'invalid_password'
+        return "ok"
+
+    if request.form['option'] == 'create':
+        user = get_db_users(username, '')
+        if user != []:
+            return 'username_busy'
+        mail = request.form['mail']
+        user = get_db_users('', mail)
+        if user != []:
+            return 'mail_busy'
+        add_db_user(username, password, mail)
+        return 'ok'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 8080)
