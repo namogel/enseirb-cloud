ed49d0472205b57963ac3bb66fecd23c43720934
benjamin <bsaintes@enseirb-matmeca.fr>
2015-01-17 15:13:24 +0100

    mise à niveau

--- flask-server/schema.sql
+++ flask-server/schema.sql
@@ -5,3 +5,11 @@ create table users (
   password text not null,
   mail text not null
 );
+drop table if exists files;
+create table files (
+  id integer primary key autoincrement,
+  id_usr integer not null,
+  folder integer not null,
+  name text not null,
+  type text not null
+);
